c179d8dbe4eed10940c1a464e426f6113c647899
Ahmed <ahmedkappo7@gmail.com>
2018-09-16 13:42:20 -0700

    add Read_single

--- models/Post.php
+++ models/Post.php
@@ -35,4 +35,31 @@ class Post{
 
         return $stmt;
     }
+
+    // Get Single Post
+    public function read_single(){
+        // Create Query
+        $query = 'SELECT c.name AS category_name, p.id,p.category_id,p.title,p.body,p.author,p.created_at 
+        From ' .$this->table.' p LEFT JOIN categories c ON p.category_id = c.id WHERE p.id = ? LIMIT 0,1';
+   
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        // execute statement
+        $stmt->execute();
+
+        $row = $stmt->fetch();
+
+        //set props
+        $this->title         = $row['title'];
+        $this->body          = $row['body'];
+        $this->author        = $row['author'];
+        $this->category_id   = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+
+    }
 }
